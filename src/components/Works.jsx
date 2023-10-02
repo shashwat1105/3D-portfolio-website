f355be4f09c6ea171694661106e8980a6c0db82f
shashwat1105 <sharmashashwat634@gmail.com>
2023-10-02 22:18:56 +0530

    lastCommit

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -22,7 +22,24 @@ className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
   <img src={image} alt={name} className='w-full h-full object-cover rounded-xl'/>
   <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
     
+
+    <div className='black-gradient  w-10 h-10 rounded-full flex
+    justify-center items-center cursor-pointer' onClick={()=>window.open(source_code_link,"_blank")}>
+      <img src={github} alt='github' className='w-1/2 h-1/2 object-contain'/>
+    </div>
   </div>
+</div>
+
+<div className='mt-5'>
+  <h3 className='text-white font-bold text-[24px] '>{name}</h3>
+  <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+</div>
+<div className='mt-4 flex flex-wrap gap-2'>
+  {tags.map((tag)=>(
+    <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+      #{tag.name}
+    </p>
+  ))}
 </div>
 </Tilt>
 </motion.div>
